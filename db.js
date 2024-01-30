0f9010aca5f637a7abd063c647825d6ad76ff051
shesh96 <shivaay9670@gmail.com>
2024-01-31 03:12:58 +0530

    again code updated

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoDB connection URL
-const mongoURL=process.env.MONGODB_URL_LOCAL;// 'hotel is name of databse , also can change'
-//const mongoURL= process.env.MONGODB_URL;
+//const mongoURL=process.env.MONGODB_URL_LOCAL;// 'hotel is name of databse , also can change'
+const mongoURL= process.env.MONGODB_URL;
 
 
 // Set up MongoDB connection
