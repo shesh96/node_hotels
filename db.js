52a50140dddbdfb3373c253041bd8760a12ef6e4
shesh96 <shivaay9670@gmail.com>
2024-01-31 00:32:46 +0530

    Shift mongodb to online server

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 // Define the MongoDB connection URL
-const mongoURL =  'mongodb://127.0.0.1:27017/hotels' // 'hotel is name of databse , also can change'
+//const mongoURL=process.env.MONGODB_URL_LOCAL;// 'hotel is name of databse , also can change'
+const mongoURL= process.env.MONGODB_URL;
+
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL);
@@ -12,7 +16,7 @@ const db=mongoose.connection;
 // Define event listenrs for database connnection 
 
 db.on('connected',()=>{ 
-    console.log('connected to MongoDB server');  
+    console.log('connected to MongoDB server');    
 });
 
 db.on('error',(err)=>{
@@ -24,5 +28,5 @@ db.on('disconnected',()=>{
 });
  
 // Export the database connection
-module.exports = db; 
+module.exports = db;   
 
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //stores in req.body
+//env file
+const PORT=process.env.PORT||3000;
 
 
 app.get('/', function (req, res) {
@@ -20,6 +23,7 @@ const menuItemRoutes=require('./routes/menuItemRoutes');
 app.use('/person',personRoutes); 
 app.use('/menuItem',personRoutes);  
 
-app.listen(3000,()=>{
+
+app.listen(PORT,()=>{
     console.log('listening on port 3000');         
 }) 
